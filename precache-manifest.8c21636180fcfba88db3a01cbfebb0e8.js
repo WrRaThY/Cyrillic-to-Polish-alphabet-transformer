3e22f36f217fdc673dd732913c3dbe70ae407625
Radoslaw Domanski <radoslaw.domanski@propellerhead.co.nz>
2019-06-21 15:34:41 +1200

    Updates

---
+++ precache-manifest.8c21636180fcfba88db3a01cbfebb0e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51febfb5e3b6a8e5b9f4b9276e0bedb3",
+    "url": "/Cyrillic-to-Polish-alphabet-transformer/index.html"
+  },
+  {
+    "revision": "d94615dbcf031f35525c",
+    "url": "/Cyrillic-to-Polish-alphabet-transformer/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "e96254039d5fb343131e",
+    "url": "/Cyrillic-to-Polish-alphabet-transformer/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d94615dbcf031f35525c",
+    "url": "/Cyrillic-to-Polish-alphabet-transformer/static/js/2.af118704.chunk.js"
+  },
+  {
+    "revision": "e96254039d5fb343131e",
+    "url": "/Cyrillic-to-Polish-alphabet-transformer/static/js/main.9c011801.chunk.js"
+  },
+  {
+    "revision": "0c22430b44490782697b",
+    "url": "/Cyrillic-to-Polish-alphabet-transformer/static/js/runtime~main.a0a8469d.js"
+  }
+]);
